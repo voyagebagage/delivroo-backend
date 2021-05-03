92dddc089a4c34aba2cc763d04559cda1e3702b9
Olivier Frugier <sedatif2@MacBook-Pro-de-Olivier.local>
2021-05-03 19:27:07 +0700

    took away ifelse condition

--- index.js
+++ index.js
@@ -452,16 +452,7 @@ app.get("/", (req, res) => {
 });
 
 // Heroku va nous fournir une variable process.env.PORT
-if (process.env.PORT) {
-  app.listen(process.env.PORT, () => {
-    console.log("Server started");
-  });
-} else {
-  app.listen(3200, () => {
-    console.log("Server launched");
-  });
-}
 
 app.listen(process.env.PORT, () => {
-  console.log("Server started");
+  console.log("Server launched");
 });
